a8d9cd8256684a0c9601542d9b3f06674f47fe69
xoss <github@xoss.org>
2014-07-17 15:31:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var base91 = require('base91');
+var prompt = require('prompt');
+var sha512 = require('sha512');
+
+var schema = {
+  properties: {
+    passphrase: {
+      hidden: true
+    },
+    id: {}
+  }
+};
+
+prompt.start();
+prompt.get(schema, function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Passphrase: ' + result.passphrase);
+  console.log('Id: ' + result.id);
+
+  var concat = result.passphrase + ' ' + result.id;
+
+  console.log('Passphrase + Id: ' + concat);
+  var hash = sha512(concat).toString('hex');
+  console.log('hash: ' + hash);
+  var encoded = base91.encode(hash);
+
+  console.log('encoded: ' + encoded.toString());
+});
